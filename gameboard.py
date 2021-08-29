009f4c9e5d0df9bb1c9a489e59e251207d8908bd
CJ1067 <38930150+CJ1067@users.noreply.github.com>
2021-08-28 23:11:37 -0400

    Uncomment rectangle code

--- gameboard.py
+++ gameboard.py
@@ -264,16 +264,16 @@ def match_img(filename):
     # Extract the coordinates of our best match
     MPx, MPy = mnLoc
 
-    # # Step 2: Get the size of the template. This is the same size as the match.
-    # trows, tcols = small_image.shape[:2]
-    #
-    # # Step 3: Draw the rectangle on large_image
-    # cv2.rectangle(online_board, (MPx, MPy), (MPx + tcols, MPy + trows), (0, 0, 255), 2)
-    #
-    # # Display the original image with the rectangle around the match.
-    # cv2.imshow('output', online_board)
-    #
-    # cv2.waitKey(0)
+    # Step 2: Get the size of the template. This is the same size as the match.
+    trows, tcols = small_image.shape[:2]
+
+    # Step 3: Draw the rectangle on large_image
+    cv2.rectangle(online_board, (MPx, MPy), (MPx + tcols, MPy + trows), (0, 0, 255), 2)
+
+    # Display the original image with the rectangle around the match.
+    cv2.imshow('output', online_board)
+
+    cv2.waitKey(0)
 
     return mnLoc, mn
 
